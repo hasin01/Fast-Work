531f4887a99a6174e9a6077438123463139436e7
hasin01 <mrsemenav5@gmail.com>
2024-11-13 15:18:27 +0200

    swiper

--- src/js/main.js
+++ src/js/main.js
@@ -22,3 +22,19 @@ document
 document
   .querySelector(".custom-next")
   .addEventListener("click", () => swiper.slideNext());
+
+  const swiper2 = new Swiper(".swiper2", {
+    modules: [Navigation],
+    slidesPerView: 1.1, 
+    loop:true,
+     centeredSlides: true, 
+      spaceBetween: 10, 
+      initialSlide:2,
+  });
+
+  document
+  .querySelector(".custom-prev2")
+  .addEventListener("click", () => swiper2.slidePrev());
+document
+  .querySelector(".custom-next2")
+  .addEventListener("click", () => swiper2.slideNext());
