186da4284f49eced8b7ebd5f3a8241f220fcd911
hasin01 <mrsemenav5@gmail.com>
2024-11-14 22:13:41 +0200

    burger-menu

--- src/js/main.js
+++ src/js/main.js
@@ -9,11 +9,11 @@ import "swiper/css/pagination";
 // init Swiper:
 const swiper = new Swiper(".swiper", {
   modules: [Navigation],
-  slidesPerView: 1.1, 
-  loop:true,
-   centeredSlides: true, 
-    spaceBetween: 10, 
-    initialSlide:2,
+  slidesPerView: 1.1,
+  loop: true,
+  centeredSlides: true,
+  spaceBetween: 10,
+  initialSlide: 2,
 });
 
 document
@@ -23,18 +23,42 @@ document
   .querySelector(".custom-next")
   .addEventListener("click", () => swiper.slideNext());
 
-  const swiper2 = new Swiper(".swiper2", {
-    modules: [Navigation],
-    slidesPerView: 1.1, 
-    loop:true,
-     centeredSlides: true, 
-      spaceBetween: 10, 
-      initialSlide:2,
-  });
+const swiper2 = new Swiper(".swiper2", {
+  modules: [Navigation],
+  slidesPerView: 1.1,
+  loop: true,
+  centeredSlides: true,
+  spaceBetween: 10,
+  initialSlide: 2,
+});
 
-  document
+document
   .querySelector(".custom-prev2")
   .addEventListener("click", () => swiper2.slidePrev());
 document
   .querySelector(".custom-next2")
   .addEventListener("click", () => swiper2.slideNext());
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.getElementById("burger");
+    const menu = document.getElementById("menu-burder");
+    const menuIcon = document.getElementById("menu-icon");
+    const overlay = document.getElementById("overlay");
+
+    const toggleMenu = () => {
+        if (menu.classList.contains('open')) {
+            menu.classList.remove('open');
+            menu.style.display = 'none';
+            menuIcon.src = './src/img/burg-menu.svg';
+            overlay.style.display = 'none';
+        } else {
+            menu.style.display = 'block';
+            setTimeout(() => menu.classList.add('open'), 10); // Немного подождем, чтобы анимация сработала
+            menuIcon.src = './src/img/burg-menu-clouse.svg';
+            overlay.style.display = 'block';
+        }
+        burger.classList.toggle('open');
+    };
+
+    burger.addEventListener("click", toggleMenu);
+});
